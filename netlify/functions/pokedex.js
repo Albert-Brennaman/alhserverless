f1eb3eb753c9bdcc4a277266366cd17e73da360e
Albert H <albert@alberthaveman.com>
2022-06-24 17:35:58 +0200

    timeout eruit gehaald

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -2,16 +2,12 @@ import fetch from "node-fetch";
 
 exports.handler = async function () {
 
-    setTimeout(() => {
+    const POKE_API = "https://pokeapi.co/api/v2/pokedex/kanto"
 
-        const POKE_API = "https://pokeapi.co/api/v2/pokedex/kanto"
-
-        const response = await fetch(POKE_API)
-        const data = await response.json();
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify(data)
-        }
-    },10000)
+    const response = await fetch(POKE_API)
+    const data = await response.json();
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+    }
 }
